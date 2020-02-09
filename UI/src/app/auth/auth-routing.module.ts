afe56d51850337f08e553f3966e236c85618b13b
Silence_JL <ma6724@vandals.uidaho.edu>
2020-02-08 17:54:26 -0800

    login html version-1

---
+++ UI/src/app/auth/auth-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from '../main/login/login.component';
+import { RestpasswordComponent } from '../main/restpassword/restpassword.component';
+import { RegisterComponent } from '../main/register/register.component';
+
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent},
+  { path: 'resetpassword', component: RestpasswordComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AuthRoutingModule { }
---
+++ UI/src/app/main/REF-README.txt
@@ -0,0 +1,16 @@
+design reference: 
+https://github.com/mike1477/ClockItApp
+https://jasonwatmore.com/post/2019/06/10/angular-8-user-registration-and-login-example-tutorial
+https://angular.io/
+
+header html:
+https://getbootstrap.com/docs/4.4/components/navbar/
+
+login html:
+https://getbootstrap.com/docs/4.4/components/forms/
+
+icon reference:
+https://fontawesome.com/
+
+
+
